4f24a3b266a021648a513fda6d9b20aef098c221
jonathangeovani <dongeovanimenor@gmail.com>
2023-01-10 22:13:47 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Getting Started with Java
+
+Here I will put some basic exercises and tests for learning and practicing purposes.
---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(sayHello("Oracle"));
+    }
+
+    private static String sayHello(String name) {
+        return "Hello " + name + "! Welcome to Java.";
+    }
+}
